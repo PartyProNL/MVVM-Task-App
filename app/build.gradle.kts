385bcf5afe680f4ca201332f40315272b8272535
Youri Scheepers <youri.scheepers@hva.nl>
2024-12-24 23:06:22 +0100

    Increase Android SDK for compiling to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "me.partypronl.mvvmtaskapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "me.partypronl.mvvmtaskapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
