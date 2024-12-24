f8c43749c6be136bdd6b1e3b30a43fddeabc627d
Youri Scheepers <youri.scheepers@hva.nl>
2024-12-24 23:55:56 +0100

    Install Hilt Navigation Compose library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.hilt.android)
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     kapt(libs.hilt.android.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
